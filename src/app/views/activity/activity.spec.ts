1becf64bd3b6d5d1e85153cb80ef47bd6abe8544
Javier Vivas <javivasv@gmail.com>
2026-07-30 18:42:45 -0400

    fix: Fix test

--- src/app/views/activity/activity.spec.ts
+++ src/app/views/activity/activity.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 
 import { Activity } from './activity';
 
@@ -11,7 +12,7 @@ describe('Activity', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Activity],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [provideHttpClient(), provideHttpClientTesting(), provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Activity);
